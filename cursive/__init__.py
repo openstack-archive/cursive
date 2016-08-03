d19043c5f501f044d8f8ba71ef4d0391b8052d08
Dane Fichter <dane.fichter@jhuapl.edu>
2016-08-03 10:36:53 -0400

    Update __init__ for better imports
    This change updates the __init__ file to
    import the signature_utils and exception
    submodules along with the cursive module.
    
    Change-Id: I6e7623e85952865fee5196cffe4e15afa18eb38e

--- cursive/__init__.py
+++ cursive/__init__.py
@@ -11,7 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+from .exception import *
+from .signature_utils import *
 import pbr.version
 
 
